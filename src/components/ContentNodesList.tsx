02135d3fa07427cf8e9608e767fc7be895ce8f24
Sina-Sche <si.scheckenbach@gmail.com>
2023-11-22 20:52:58 +0100

    feat: add getContentNodes Query and list

---
+++ src/components/ContentNodesList.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@apollo/client";
+import {
+  ContentNode,
+  GET_CONTENT_NODES,
+  ContentNodesResponse,
+} from "../graphql/getContentNodes";
+import placeholderImage from "../assets/placeholder.png";
+import { Img, ListElement, Title } from "./ContentNodesListStyles";
+import { User } from "../types";
+import { v4 as uuidv4 } from "uuid";
+
+const ContentNodesList: React.FC<User> = ({ token }) => {
+  const { loading, error, data } = useQuery<ContentNodesResponse>(
+    GET_CONTENT_NODES,
+    {
+      context: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    }
+  );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  const nodes: ContentNode[] | undefined =
+    data?.Admin.Tree.GetContentNodes.edges.map((edge: any) => edge.node);
+
+  return (
+    <ul>
+      {nodes?.map((node: any) => (
+        <ListElement key={uuidv4()}>
+          <Title>{node.structureDefinition.title}</Title>
+          {node.image ? (
+            <Img
+              src={node.image}
+              alt="lesson"
+              height={"30vh"}
+              width={"100px"}
+            />
+          ) : (
+            <Img src={placeholderImage} alt="lesson" />
+          )}
+        </ListElement>
+      ))}
+    </ul>
+  );
+};
+
+export default ContentNodesList;
---
+++ src/components/ContentNodesListStyles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ListElement = styled.li`
+  margin: ${(props) => props.theme.spacing.large};
+`;
+
+export const Title = styled.p`
+  font-weight: bold;
+`;
+
+export const Img = styled.img`
+  width: 200px;
+`;
